ac249d95e40804896d5c8f497e1c93e4bbb4f170
Eugeny Tatarincev <et@virtoway.com>
2022-06-22 17:48:51 +0200

    fix compilation errors

--- src/VirtoCommerce.ImportModule.CsvHelper/CsvDataReader.cs
+++ src/VirtoCommerce.ImportModule.CsvHelper/CsvDataReader.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using CsvHelper;
 using CsvHelper.Configuration;
 using VirtoCommerce.ImportModule.Core.Models;
