e3cddfe9bbf3c709ed27437db44838ae9ad0ac2c
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2023-06-20 10:31:02 +0200

    VM-1173: indexes in ImportRunHistory table

---
+++ src/VirtoCommerce.ImportModule.Data/Migrations/20230615115744_AddRunHistoryIndexes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.ImportModule.Data.Migrations
+{
+    public partial class AddRunHistoryIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ImportRunHistory_JobId",
+                table: "ImportRunHistory",
+                column: "JobId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImportRunHistory_ProfileId",
+                table: "ImportRunHistory",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImportRunHistory_UserId",
+                table: "ImportRunHistory",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ImportRunHistory_JobId",
+                table: "ImportRunHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImportRunHistory_ProfileId",
+                table: "ImportRunHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImportRunHistory_UserId",
+                table: "ImportRunHistory");
+        }
+    }
+}
