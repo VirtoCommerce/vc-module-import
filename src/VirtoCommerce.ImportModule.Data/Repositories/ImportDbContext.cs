320ed162f76ae9c81af645e1ac70df71baa85673
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2023-06-29 17:04:34 +0200

    VM-1178: Erorrs serialization/deserialization in ImportRunHistoryEntity

--- src/VirtoCommerce.ImportModule.Data/Repositories/ImportDbContext.cs
+++ src/VirtoCommerce.ImportModule.Data/Repositories/ImportDbContext.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using EntityFrameworkCore.Triggers;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 using VirtoCommerce.ImportModule.Data.Models;
 
 namespace VirtoCommerce.ImportModule.Data.Repositories
@@ -28,8 +26,8 @@ namespace VirtoCommerce.ImportModule.Data.Repositories
             #region ImportRunHistoryEntity
             modelBuilder.Entity<ImportRunHistoryEntity>().ToTable("ImportRunHistory").HasKey(x => x.Id);
             modelBuilder.Entity<ImportRunHistoryEntity>().Property(x => x.Id).HasMaxLength(128).ValueGeneratedOnAdd();
-            modelBuilder.Entity<ImportRunHistoryEntity>().Property(x => x.Errors)
-            .HasConversion(y => JsonConvert.SerializeObject(y), y => JsonConvert.DeserializeObject<ICollection<string>>(y));
+            modelBuilder.Entity<ImportRunHistoryEntity>().Property(x => x.Errors);
+            //.HasConversion(y => JsonConvert.SerializeObject(y), y => JsonConvert.DeserializeObject<ICollection<string>>(y));
             modelBuilder.Entity<ImportRunHistoryEntity>().HasIndex(x => x.ProfileId);
             modelBuilder.Entity<ImportRunHistoryEntity>().HasIndex(x => x.UserId);
             modelBuilder.Entity<ImportRunHistoryEntity>().HasIndex(x => x.JobId);
