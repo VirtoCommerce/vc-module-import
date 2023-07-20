aa00c0c84dfb8a3723c8c9eeccc0fdc1c24da400
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2023-07-20 12:56:33 +0200

    VM-1176: attempt to reduce serialized ImportProfile in job

--- src/VirtoCommerce.ImportModule.Data/Services/DataImportProcessManager.cs
+++ src/VirtoCommerce.ImportModule.Data/Services/DataImportProcessManager.cs
@@ -129,7 +129,7 @@ namespace VirtoCommerce.ImportModule.Data.Services
             importProgress.Description = "Import has been finished";
             importProgress.Finished = DateTime.UtcNow;
             importProgress.ReportUrl = errorReportResult;
-            await context.CompleteCallback();
+            //await context.CompleteCallback();
 
             await progressCallback(importProgress);
         }
