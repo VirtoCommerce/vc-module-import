4bc03ef27eeaf55f5d1a70655144bfc7e458cfea
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2024-03-19 10:07:03 +0100

    VM-1294: unescape import file url 2

--- src/VirtoCommerce.ImportModule.Data/Services/ImportRunService.cs
+++ src/VirtoCommerce.ImportModule.Data/Services/ImportRunService.cs
@@ -170,6 +170,8 @@ namespace VirtoCommerce.ImportModule.Data.Services
         public async Task<ImportDataPreview> PreviewAsync(ImportProfile importProfile)
         {
             var importer = _dataImporterFactory.Create(importProfile.DataImporterType);
+            importProfile.ImportFileUrl = Uri.UnescapeDataString(importProfile.ImportFileUrl);
+
             var context = new ImportContext(importProfile);
 
             var result = new ImportDataPreview();
@@ -202,6 +204,8 @@ namespace VirtoCommerce.ImportModule.Data.Services
         public async Task<ValidationResult> ValidateAsync(ImportProfile importProfile)
         {
             var importer = _dataImporterFactory.Create(importProfile.DataImporterType);
+            importProfile.ImportFileUrl = Uri.UnescapeDataString(importProfile.ImportFileUrl);
+
             var context = new ImportContext(importProfile);
 
             var validationResult = await importer.ValidateAsync(context);
