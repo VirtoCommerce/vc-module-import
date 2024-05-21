3f93f595d8c8873c30323ea77a195c29f5b2b667
symbot <maxim.zinchuk@gmail.com>
2024-05-21 16:43:13 +0300

    chore: update vc-shell to version 1.0.218

--- src/VirtoCommerce.ImportModule.Web/import-app/src/modules/import/pages/import-profile-selector.vue
+++ src/VirtoCommerce.ImportModule.Web/import-app/src/modules/import/pages/import-profile-selector.vue
@@ -78,6 +78,18 @@
           <template #item_finished="itemData">
             <ImportStatus :item="itemData.item" />
           </template>
+
+          <template #empty>
+            <div class="tw-w-full tw-h-full tw-box-border tw-flex tw-flex-col tw-items-center tw-justify-center">
+              <VcIcon
+                icon="fas fa-file-import"
+                class="!tw-text-[95px] !tw-text-[#329ED4]"
+              />
+              <div class="tw-m-4 tw-text-xl tw-font-medium">
+                {{ $t("IMPORT.PAGES.PROFILE_SELECTOR.EMPTY.NO_ITEMS") }}
+              </div>
+            </div>
+          </template>
         </VcTable>
       </VcCard>
     </VcContainer>
