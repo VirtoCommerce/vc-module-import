c4f2735830b039b0ccbe9ca776e64e3ee572d2b7
Maksim Zinchuk <maxim.zinchuk@gmail.com>
2024-08-22 15:31:49 +0300

    refactor: Add cross-spawn dependency for better process spawning

--- src/VirtoCommerce.ImportModule.Web/import-app/scripts/release.ts
+++ src/VirtoCommerce.ImportModule.Web/import-app/scripts/release.ts
@@ -1,15 +1,15 @@
 import { release } from "@vc-shell/release-config";
-import { spawnSync } from "node:child_process";
+import { sync } from "cross-spawn";
 
 release({
   packages: [".", "src/api_client", "src/modules"],
   toTag: (version) => `v${version}`,
   bumpVersion: async (pkgName, pkgVersion) => {
     const bumpArgs = ["workspace", pkgName, "version", pkgVersion, "--deferred"];
-    await spawnSync("yarn", bumpArgs);
+    await sync("yarn", bumpArgs);
 
     const versionApplyArgs = ["version", "apply", "--all"];
-    await spawnSync("yarn", versionApplyArgs);
+    await sync("yarn", versionApplyArgs);
   },
   generateChangelog: async () => {},
 });
